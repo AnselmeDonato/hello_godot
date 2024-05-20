1b7c1a212faf916bff1005ea40a7a1963176d69a
Anselme Donato <anselmedonato@anselmes-macbook-pro.home>
2024-05-20 22:41:56 +0200

    true random wave function collapse

--- level/tile.gd
+++ level/tile.gd
@@ -1,9 +1,87 @@
 extends Node2D
 
-func load_texture(texture_path: String):
-	var texture = load(texture_path)
+# ================== Variables ================== #
+
+const ASSETS_PATH = "res://level/assets/"
+
+var possible_tiles = {}
+var possible_neighbors = {
+	"up": {},
+	"down": {},
+	"left": {},
+	"right": {}
+}
+
+var tiles_rules = {}
+
+# ================== Functions ================== #
+
+func get_possible_tiles():
+	return possible_tiles
+
+
+
+func get_possible_neighbors():
+	return possible_neighbors
+
+
+
+func is_collapsed():
+	if possible_tiles.size() == 1:
+		return true
+	return false
+
+
+
+func load_texture():
+	if not is_collapsed():
+		print("Error: can't load texture if the tile is not collapsed")
+		return 1
+	
+	var texture_file = tiles_rules[possible_tiles.keys()[0]]['file']
+	var texture = load(ASSETS_PATH + texture_file)
 	$Sprite2D.texture = texture
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+
+
+func load_possible_tiles():
+	var tiles_rules_path = "res://level/assets/tiles_rules.json"
+
+	var tiles_rules_string = FileAccess.get_file_as_string(tiles_rules_path)
+	var open_error = FileAccess.get_open_error()
+	if open_error > 0:
+		print("FileAccess open Error: at file ", tiles_rules_path)
+
+	var json = JSON.new()
+	var error = json.parse(tiles_rules_string)
+	if error > 0:
+		print("JSON parse Error: ", json.get_error_message(), tiles_rules_string, "at line ", json.get_error_line())
+		return 1
+
+	tiles_rules = json.data
+
+	for possible_tile in tiles_rules.keys():
+		possible_tiles[possible_tile] = 1
+
+		possible_neighbors['up'].merge(tiles_rules[possible_tile]['possible_neighbors']['up'])
+		possible_neighbors['down'].merge(tiles_rules[possible_tile]['possible_neighbors']['down'])
+		possible_neighbors['left'].merge(tiles_rules[possible_tile]['possible_neighbors']['left'])
+		possible_neighbors['right'].merge(tiles_rules[possible_tile]['possible_neighbors']['right'])
+
+
+
+func collapse():
+	print('here')
+	var rng = RandomNumberGenerator.new() 
+	var possible_tile_names = possible_tiles.keys()
+	var collapsed_tile_name = possible_tile_names[rng.randi_range(0, possible_tile_names.size() - 1)]
+	var collapsed_tile = tiles_rules[collapsed_tile_name]
+
+	possible_tiles = {collapsed_tile_name: collapsed_tile}
+	print(possible_tiles.size())
+
+
+
+# Called when when the object's script is instantiated.
+func _init():
+	load_possible_tiles()
--- main.gd
+++ main.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var tile_scene: PackedScene
 
+# ================== Variables ================== #
+
 const FORMAT_TEXTURE_PATH = "res://level/assets/%s"
 
 const RED_TILE_INT = 0
@@ -13,16 +15,17 @@ const GRID_DIMENSTION = 10
 var grid = []
 var tiles_to_collapse_position = []
 
-# Initialize map grid with dictionnaries of all possible tiles
-func initialize_grid(grid_dimentsion: int):
-	var all_possible_tiles = [RED_TILE_INT, BLUE_TILE_INT]
-	all_possible_tiles.sort()
+# ================== Functions ================== #
 
-	for y in range(grid_dimentsion):
+# Initialize map grid with dictionnaries of all possible tiles
+func initialize_grid(grid_dimension: int):
+	for y in range(grid_dimension):
 		var row = []
-		for x in range(grid_dimentsion):
-			row.append(all_possible_tiles)
-			tiles_to_collapse_position.append([x,y])
+		for x in range(grid_dimension):
+			var tile = tile_scene.instantiate()
+			row.append(tile)
+
+			tiles_to_collapse_position.append([x, y])
 		grid.append(row)
 
 
@@ -32,21 +35,14 @@ func collapse_grid():
 	while tiles_to_collapse_position.size() > 0:
 		var tile_position = tiles_to_collapse_position.pop_at(rng.randi_range(0, tiles_to_collapse_position.size() - 1))
 		var tile = grid[tile_position[0]][tile_position[1]]
-		var collapsed_tile = tile[rng.randi_range(0, tile.size() - 1)]
-
-		grid[tile_position[0]][tile_position[1]] = collapsed_tile
+		tile.collapse()
+		grid[tile_position[0]][tile_position[1]] = tile
 
 func show_grid():
 	for y in range(grid.size()):
 		for x in range(grid[0].size()):
-			var tile = tile_scene.instantiate()
-
-			match grid[x][y]:
-				RED_TILE_INT:
-					tile.load_texture(FORMAT_TEXTURE_PATH % "red_256.png")
-				BLUE_TILE_INT:
-					tile.load_texture(FORMAT_TEXTURE_PATH % "blue_256.png")
-
+			var tile = grid[y][x]
+			tile.load_texture()
 			tile.position = Vector2(x * TILE_DIMENSION, y * TILE_DIMENSION)
 			add_child(tile)
 
@@ -58,5 +54,5 @@ func build_tile_grid(grid_dimension: int):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	build_tile_grid(10)
+	build_tile_grid(GRID_DIMENSTION)
 
