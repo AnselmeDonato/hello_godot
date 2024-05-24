0e4584a9701ded21a475e411f8cb95ad8e64aabd
Anselme Donato <anselmedonato@anselmes-macbook-pro.home>
2024-05-24 10:36:11 +0200

    refacto + center player

--- level/grid.gd
+++ level/grid.gd
@@ -4,18 +4,28 @@ var tile_scene = preload("res://level/tile.tscn")
 
 # ================== Variables ================== #
 
-const TILES_RULES_PATH = "res://level/assets/tiles_rules.json"
-const TILE_DIMENSION = 512
-const GRID_DIMENSTION = 10
+var TILES_RULES_PATH = "res://level/assets/tiles_rules.json"
+var TILE_DIMENSION = 512
+var GRID_DIMENSION = 10
 
 var tiles_rules = {}
 var grid = []
 
-# ================== Functions ================== #
+# ================== "Private" Functions ================== #
 
 func tile_at(pos: Array):
 	"""Return the tile at a position in the grid (it's NOT godot's own 'position')"""
 	return grid[pos[1]][pos[0]]
+
+
+func is_collapsed():
+	"""Return true if all the tiles in the grid have collapsed to a single state"""
+	for y in range(grid.size()):
+		for x in range(grid[0].size()):
+			if not tile_at([x,y]).is_collapsed():
+				return false
+	
+	return true
 
 
 func get_smallest_entropy_pos():
@@ -39,22 +49,34 @@ func get_smallest_entropy_pos():
 	return smallest_entropy_pos
 
 
-func is_collapsed():
-	"""Return true if all the tiles in the grid have collapsed to a single state"""
-	for y in range(grid.size()):
-		for x in range(grid[0].size()):
-			if not tile_at([x,y]).is_collapsed():
-				return false
-	
-	return true
+func get_superposed_neighbors_pos(from_pos):
+	"""Return a list of the position of superposed (i.e not collapsed) 1-neighbors of the position from_pos"""
+	var superposed_neighbors_pos = []
 
+	# Sorry for the ugly naming: right here there is going to be a confusion between "_pos"=position, 
+	# used everywhere else in grid.gd, and "pos_"=positive, used for the constraints in tiles_rules.json
+	# I'll find something better one day, but for now let's stick to the confusion
+	if from_pos[0] > 0:
+		var neg_x_pos = [from_pos[0] - 1, from_pos[1]]
+		if not tile_at(neg_x_pos).is_collapsed():
+			superposed_neighbors_pos.append(neg_x_pos)
 
-func collapse():
-	"""Collapse the grid by collapsing each tile and propagating the result"""
-	while not is_collapsed():
-		var pos_to_collapse = get_smallest_entropy_pos()
-		tile_at(pos_to_collapse).collapse()
-		propagate_collapse(pos_to_collapse)
+	if from_pos[0] < GRID_DIMENSION - 1:
+		var pos_x_pos = [from_pos[0] + 1, from_pos[1]]
+		if not tile_at(pos_x_pos).is_collapsed():
+			superposed_neighbors_pos.append(pos_x_pos)
+
+	if from_pos[1] > 0:
+		var neg_y_pos = [from_pos[0], from_pos[1] - 1]
+		if not tile_at(neg_y_pos).is_collapsed():
+			superposed_neighbors_pos.append(neg_y_pos)
+
+	if from_pos[1] < GRID_DIMENSION - 1:
+		var pos_y_pos = [from_pos[0], from_pos[1] + 1]
+		if not tile_at(pos_y_pos).is_collapsed():
+			superposed_neighbors_pos.append(pos_y_pos)
+
+	return superposed_neighbors_pos
 
 
 func propagate_collapse(from_pos: Array):
@@ -88,60 +110,7 @@ func propagate_collapse(from_pos: Array):
 				stack.append(neighbor_pos)
 
 
-func get_superposed_neighbors_pos(from_pos):
-	"""Return a list of the position of superposed (i.e not collapsed) 1-neighbors of the position from_pos"""
-	var superposed_neighbors_pos = []
-
-	# Sorry for the ugly naming: right here there is going to be a confusion between "_pos"=position, 
-	# used everywhere else in grid.gd, and "pos_"=positive, used for the constraints in tiles_rules.json
-	# I'll find something better one day, but for now let's stick to the confusion
-	if from_pos[0] > 0:
-		var neg_x_pos = [from_pos[0] - 1, from_pos[1]]
-		if not tile_at(neg_x_pos).is_collapsed():
-			superposed_neighbors_pos.append(neg_x_pos)
-
-	if from_pos[0] < GRID_DIMENSTION - 1:
-		var pos_x_pos = [from_pos[0] + 1, from_pos[1]]
-		if not tile_at(pos_x_pos).is_collapsed():
-			superposed_neighbors_pos.append(pos_x_pos)
-
-	if from_pos[1] > 0:
-		var neg_y_pos = [from_pos[0], from_pos[1] - 1]
-		if not tile_at(neg_y_pos).is_collapsed():
-			superposed_neighbors_pos.append(neg_y_pos)
-
-	if from_pos[1] < GRID_DIMENSTION - 1:
-		var pos_y_pos = [from_pos[0], from_pos[1] + 1]
-		if not tile_at(pos_y_pos).is_collapsed():
-			superposed_neighbors_pos.append(pos_y_pos)
-
-	return superposed_neighbors_pos
-
-func show():
-	"""Show the grid by loading textures of all the tiles"""
-	for y in range(grid.size()):
-		for x in range(grid[0].size()):
-			var tile = tile_at([x,y])
-			tile.load_texture()
-			tile.position = Vector2(x * TILE_DIMENSION, y * TILE_DIMENSION)
-			add_child(tile)
-
-
-func init_grid():
-	"""Initialise the grid with all its tiles"""
-	var tile_ref = tile_scene.instantiate()
-	var all_possible_tiles = {}
-	for tile in tiles_rules.keys():
-		all_possible_tiles[tile] = 1
-
-	for y in range(GRID_DIMENSTION):
-		var row = []
-		for x in range(GRID_DIMENSTION):
-			var tile = tile_ref.duplicate()
-			tile.set_superposition(all_possible_tiles.duplicate())
-			row.append(tile)
-
-		grid.append(row)
+# ====================== "Public" functions ====================== #
 
 func load_tiles_rules():
 	"""Load the rules for tiles adjacency from a json file"""
@@ -159,15 +128,50 @@ func load_tiles_rules():
 	tiles_rules = json.data
 
 
-# Called when the node enters the scene tree for the first time.
+func init_grid():
+	"""Initialise the grid with all its tiles"""
+	var tile_ref = tile_scene.instantiate()
+	var all_possible_tiles = {}
+	for tile in tiles_rules.keys():
+		all_possible_tiles[tile] = 1
+
+	for y in range(GRID_DIMENSION):
+		var row = []
+		for x in range(GRID_DIMENSION):
+			var tile = tile_ref.duplicate()
+			tile.set_superposition(all_possible_tiles.duplicate())
+			row.append(tile)
+
+		grid.append(row)
+
+
 func _init():
+	"""Called when the node enters the scene tree for the first time"""
 	load_tiles_rules()
 	init_grid()
+
+
+func collapse():
+	"""Collapse the grid by collapsing each tile and propagating the result"""
+	while not is_collapsed():
+		var pos_to_collapse = get_smallest_entropy_pos()
+		tile_at(pos_to_collapse).collapse()
+		propagate_collapse(pos_to_collapse)
+
+
+func show():
+	"""Show the grid by loading textures of all the tiles"""
+	for y in range(grid.size()):
+		for x in range(grid[0].size()):
+			var tile = tile_at([x,y])
+			tile.load_texture()
+			tile.position = Vector2(x * TILE_DIMENSION, y * TILE_DIMENSION)
+			add_child(tile)
 
 
 # =========== For debug ============= #
 
 func print_every_superposition():
-	for x in range(GRID_DIMENSTION):
-		for y in range(GRID_DIMENSTION):
+	for x in range(GRID_DIMENSION):
+		for y in range(GRID_DIMENSION):
 			print(tile_at([x,y]).superposition)
--- main.gd
+++ main.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var grid_scene = preload("res://level/grid.tscn")
+var player_scene = preload("res://player/player.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +10,9 @@ func _ready():
 	grid.collapse()
 	grid.show()
 	add_child(grid)
+
+	var player = player_scene.instantiate()
+	var center_grid = Vector2(grid.TILE_DIMENSION * grid.GRID_DIMENSION / 2, grid.TILE_DIMENSION * grid.GRID_DIMENSION / 2)
+	player.set_position(center_grid)
+	add_child(player)
 
